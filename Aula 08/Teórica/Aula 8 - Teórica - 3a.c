82784bd83f80ac96fb95bfb46c09c1fd318a320d
Fellipe de Sá Moraes <124213206+Fellipz1@users.noreply.github.com>
2023-05-10 14:50:08 -0300

    Update Aula 8 - Teórica - 3a.c

--- Aula 08/Teórica/Aula 8 - Teórica - 3a.c
+++ Aula 08/Teórica/Aula 8 - Teórica - 3a.c
@@ -1,2 +1,20 @@
-// 3 - a) Escreva uma função que recebe um número inteiro n como parâmetro e imprime a tabuada de n. Por exemplo, se n for 3, a função deve imprimir as linhas do quadro à direita.
+// 3 - a) Escreva uma funÃ§Ã£o que recebe um nÃºmero inteiro n como parÃ¢metro e imprime a tabuada de n. Por exemplo, se n for 3, a funÃ§Ã£o deve imprimir as linhas do quadro Ã  direita.
 
+#include <stdio.h>
+
+void tabuada(int n) {
+    int i;
+    for (i = 1; i <= 10; i++) {
+        printf("%d x %d = %d\n", n, i, n*i);
+    }
+}
+int main() {
+    int valor;
+    while (1) {
+        printf("Digite um valor inteiro: ");
+        scanf("%d", &valor);
+        tabuada(valor);
+        break;
+    }
+    return 0;
+}
