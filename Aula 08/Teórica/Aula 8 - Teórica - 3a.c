2a27563d6899d61380f3e7934a5c1fcfbcef99f7
Fellipe de Sá Moraes <124213206+Fellipz1@users.noreply.github.com>
2023-05-10 17:56:32 -0300

    Update Aula 8 - Teórica - 3a.c

--- Aula 08/Teórica/Aula 8 - Teórica - 3a.c
+++ Aula 08/Teórica/Aula 8 - Teórica - 3a.c
@@ -3,15 +3,17 @@
 #include <stdio.h>
 
 void tabuada(int n) {
-    int i;
-    for (i = 1; i <= 10; i++) {
-        printf("%d x %d = %d\n", n, i, n*i);
+    int i = 1;
+    while (i <= 10) {
+        printf("%d x %d = %d\n", n, i, n * i);
+        i++;
     }
 }
+
 int main() {
     int valor;
     while (1) {
-        printf("Digite um valor inteiro: ");
+    	printf("Digite um valor inteiro: ");
         scanf("%d", &valor);
         tabuada(valor);
         break;
