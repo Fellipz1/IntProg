8a7d5f648b98014bc255d73b89a40b089f38180e
Fellipe de Sá Moraes <124213206+Fellipz1@users.noreply.github.com>
2023-05-10 13:32:09 -0300

    Update Aula 8 - Teórica -  2.c

--- Aula 08/Teórica/Aula 8 - Teórica -  2.c
+++ Aula 08/Teórica/Aula 8 - Teórica -  2.c
@@ -4,16 +4,17 @@
 Linha	|	a	|	a	|	b	|	Condicao1	|	Condicao 2
 	13	|	?	|	?	|	?	|				|				|
 	15	|	4	|	?	|	?	|				|				|
-	2	|		|	4	|	?	|				|				|		//Imprime: Quantas vezes a impressao deve ser feita?
+	2	|		|	4	|	?	|				|				|	
 	3	|		|	4	|	1	|				|				|
 	4	|		|	4	|	1	|		V		|				|
 	5	|		|	4	|	1	|				|		F		|
-	9	|		|	4	|	2	|				|				|		//Imprime: Faca o teste de mesa dos exercicios
+	9	|		|	4	|	2	|				|				|		
 	4	|		|	4	|	2	|		V		|				|
 	5	|		|	4	|	2	|				|		F		|
-	9	|		|	4	|	3	|				|				|		//Imprime: Faca o teste de mesa dos exercicios
+	9	|		|	4	|	3	|				|				|	
 	4	|		|	4	|	3	|		V		|				|
 	5	|		|	4	|	3	|				|		V		|
 	9	|		|	4	|	4	|				|				|
 	4	|		|	4	|	4	|		F		|				|
+	O que foi impresso durante o programa: A5A6 e depois 3
 */
