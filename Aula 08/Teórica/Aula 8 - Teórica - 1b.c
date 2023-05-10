05ee1a4b39f38d41755118588eb15e7d34d0b3a4
Fellipe de Sá Moraes <124213206+Fellipz1@users.noreply.github.com>
2023-05-10 14:10:06 -0300

    Update Aula 8 - Teórica - 1b.c

--- Aula 08/Teórica/Aula 8 - Teórica - 1b.c
+++ Aula 08/Teórica/Aula 8 - Teórica - 1b.c
@@ -1,42 +1,45 @@
-// b) Reescreva o programa para que ele repita a leitura e impressão para um grupo de alunos. A quantidade de notas lidas é desconhecida. O programa deve ser encerrado se for lido um valor inválido (fora do intervalo entre 0 e 100). Teste seu programa com a sequência: 87 59 94 108
+// b) Reescreva o programa para que ele repita a leitura e impressÃ£o para um grupo de alunos. A quantidade de notas lidas Ã© desconhecida. O programa deve ser encerrado se for lido um valor invÃ¡lido (fora do intervalo entre 0 e 100). Teste seu programa com a sequÃªncia: 87 59 94 108
 
 #include <stdio.h>
-
-void situacao(float nota) 
-{
-    while (nota>=0 && nota<=100) {
-        
-    	if(nota>=70 && nota<101)
-		{
-			printf("Aprovado\n");
-			break;
-		}
-		if (nota < 70 && nota>=0)
-		{
-			printf("Reprovado\n");
-			break;
-		}
-    }
-}
-
 int main()
 {
-	int valor=1;
-	float nota;
-	   
-    while (valor!=0)
+    float n;
+    
+    printf("Digite sua nota: ");
+    scanf ("%f",&n);
+    
+    if (n>=70 && n<=100)
     {
-    	printf("Digite 0 para fechar o programa e 1 para continuar \n");
-    	scanf("%d", &valor);
-    	
-    	if(valor == 1){
-    		printf("Digite a nota do aluno: \n");
-   			scanf("%f", &nota);
-			situacao(nota);
-		}
-		else {
-			break;
-		}
-	}
-    return 0;
+		printf("Aprovado\n");
+    }
+      	else {
+        	if (n<70 && n>=0){
+              printf("Reprovado\n");
+          	}
+          	else {
+              printf ("Nota invalida\n");
+          	}
+      	}
+   while (n>=0 && n <=100)
+    {
+        float nota;
+        
+        printf("Digite outra nota: ");
+        scanf("%f", &nota);
+        
+        if (nota>=70 && nota<=100)
+        {
+        	printf("Aprovado\n");
+        }
+      	else {
+          	if (nota <70 && n>=0) {
+              printf("Reprovado\n");
+          	}
+          	else {
+              printf("Nota invalida");
+              break;
+            }
+      	}
+    }
+      return 0;
 }
