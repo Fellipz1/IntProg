a072ccbc7cbfb42dd5d705b85475eaa6d821cd77
Fellipe de Sá Moraes <124213206+Fellipz1@users.noreply.github.com>
2023-05-10 14:50:48 -0300

    Update Aula 8 - Teórica - 3b.c

--- Aula 08/Teórica/Aula 8 - Teórica - 3b.c
+++ Aula 08/Teórica/Aula 8 - Teórica - 3b.c
@@ -1,2 +1,20 @@
-// b) Faça um programa que leia um valor inteiro do teclado e use a função da letra a para imprimir a tabuada deste número.
+// b) FaÃ§a um programa que leia um valor inteiro do teclado e use a funÃ§Ã£o da letra a para imprimir a tabuada deste nÃºmero.
 
+#include <stdio.h>
+
+void tabuada(int n) {
+    int i;
+    for (i = 1; i <= 10; i++) {
+        printf("%d x %d = %d\n", n, i, n*i);
+    }
+}
+int main() {
+    int valor;
+    while (1) {
+        printf("Digite um valor inteiro: ");
+        scanf("%d", &valor);
+        tabuada(valor);
+        break;
+    }
+    return 0;
+}
