c81438f92803469d62468c13866ad2c4332b36c9
Fellipe de Sá Moraes <124213206+Fellipz1@users.noreply.github.com>
2023-05-10 14:52:14 -0300

    Update Aula 8 - Teórica - 3c.c

--- Aula 08/Teórica/Aula 8 - Teórica - 3c.c
+++ Aula 08/Teórica/Aula 8 - Teórica - 3c.c
@@ -1,2 +1,23 @@
-// c) Reescreva o programa para que ele imprima a tabuada de todo valor inteiro que o usu·rio digitar. O programa dever· ser encerrado quando o usu·rio digitar o valor zero.
+// c) Reescreva o programa para que ele imprima a tabuada de todo valor inteiro que o usu√°rio digitar. O programa dever√° ser encerrado quando o usu√°rio digitar o valor zero.
 
+#include <stdio.h>
+
+void tabuada(int n) {
+    int i;
+    for (i = 1; i <= 10; i++) {
+        printf("%d x %d = %d\n", n, i, n*i);
+    }
+}
+
+int main() {
+    int valor;
+    while (1) {
+        printf("Digite um valor inteiro (ou 0 para sair): ");
+        scanf("%d", &valor);
+        if (valor == 0) {
+            break;
+        }
+        tabuada(valor);
+    }
+    return 0;
+}
