ea75299fc501395e94c0aa11a5368ecabbcd18e1
Fellipe de Sá Moraes <124213206+Fellipz1@users.noreply.github.com>
2023-05-31 19:14:46 -0300

    Update Questão 3.c

--- Aula 11/Teórica/Questão 3.c
+++ Aula 11/Teórica/Questão 3.c
@@ -2,26 +2,23 @@
 
 #include <stdio.h>
 
-int calcularFatorial(int n) {
-    int fatorial = 1;
-    
-    for (int i = 1; i <= n; i++) {
-        fatorial *= i;
+float fatorial(int n) {
+    int valor_final = n;
+
+    for (int i = 1; i < n; i++) 
+	{
+        valor_final = (valor_final * (n - i));
     }
     
-    return fatorial;
+    return valor_final;
 }
 
 int main() {
-    int numero;
-    int resultado;
-    
+    int n, fat;
     printf("Digite um valor para obter o seu fatorial: \n");
-	scanf("%d",&numero);
-	
-    resultado = calcularFatorial(numero);
-    
-    printf("O fatorial de %d =: %d\n", numero, resultado);
+    scanf("%d", &n);
+    fat = fatorial(n);
+    printf("O valor do seu %d fatorial = %d\n", n, fat);
     
     return 0;
 }
