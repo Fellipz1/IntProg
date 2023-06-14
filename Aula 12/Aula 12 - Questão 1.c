413f2f5eb3f8e5736ced7b2e03dde98e6db9e3f1
Fellipe de Sá Moraes <124213206+Fellipz1@users.noreply.github.com>
2023-06-13 22:46:09 -0300

    Delete Aula 12 directory

--- Aula 12/Aula 12 - Questão 1.c
+++
@@ -1,5 +0,0 @@
-// Questão 1 
-vet |1|2|4|7|4|2|8|9|0|6|5|4|3|
-Quais são os elementos do vetor referenciados por: vet[3]? vet[0]? vet[13]
-Resposta vet[3]= 7 ; vet[0]= 1 ; vet[13]= não existe no vetor proposto pelo exercício;
-
