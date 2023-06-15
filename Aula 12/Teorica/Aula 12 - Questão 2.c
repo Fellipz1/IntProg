7d8b0212a807cea70c1c6133539ea45dc9a62718
Fellipe de Sá Moraes <124213206+Fellipz1@users.noreply.github.com>
2023-06-15 20:44:50 -0300

    Delete Aula 12 directory

--- Aula 12/Teorica/Aula 12 - Questão 2.c
+++
@@ -1,6 +0,0 @@
-// Questão 2 
-Quais as modificações feitas no vetor em:
-vet[1]=vet[3]*vet[1] = Nesse caso, o vetor na posição 3 do vetor é mulltiplicado pelo vaolor do elemento na posição 1, o resultado da operação fica armazenado na posição 1 do vetor. Não falando em vetor temos nosso quarto valor sendo multiplicado pelo segundo e o resultado sendo o novo valor do segundo número digitado pelo usuario.
-vet[10]=pow(vet[2],2)+vet[0] = Aqui o vetor do elemento na posição 2 do vetor é elevado ao quadrado usando a função pow, após isso o resultado é somado ao valor do elemento na posição 0 do vetor, o resultado final é atribuído à posição 10 do vetor.
-vet[9]=vet[3]<vet[4] = Nesse temos a expressão vet[3]<vet[4] sendo avaliada. Se o valor do vet[3] for menor que o de vet[4], temos um resultado 1(verdadeiro), se não temos então um resultado 0 (negativo), o resultado é atribuido à posição 9 do vetor.
-vet[6]=vet[vet[5]] = Aqui, o valor do elemento na posição 5 do vetor é usado como índice para acessar o valor correspondente no vetor. Esse valor resultante é então atribuído ao vetor de posição 6.
--- Aula 12/Teorica/Aula 12 - Questão 3.c
+++
@@ -1,4 +0,0 @@
-// Questão 3 
-Qual é a diferença entre os números "3" das duas instruções abaixo?
-int vet[3]; -> Aqui declaramos que teremos e elementos no vetor que estamos declarando.
-vet[3]=5; -> Aqui estamos alterando o valor do nosso quarto vetor.
